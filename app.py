f147b0bc4c2b144e40f6d339b52ae882928dfeb9
Tanvi1643 <117074997+Tanvi1643@users.noreply.github.com>
2024-06-09 23:17:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def Index():
 def Summarize():
     if req.method== "POST":
         API_URL = "https://api-inference.huggingface.co/models/facebook/bart-large-cnn"
-        headers = {"Authorization": f"Bearer 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"}
+        headers = {"Authorization": f"Bearer api key"}
 
         data=req.form["data"]
 
